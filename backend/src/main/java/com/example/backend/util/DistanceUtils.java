5a95131781dbb22c4557613f89abee585951247f
Chakravarthi Repalle <chakravarthi.devcode.625@gmail.com>
2026-09-21 10:13:16 +0530

    Add DistanceUtils for distance calculations
    This utility class provides methods to calculate the Euclidean distance between two locations and to build a distance matrix for a list of locations.

---
+++ backend/src/main/java/com/example/backend/util/DistanceUtils.java
@@ -0,0 +1,28 @@
+package com.example.backend.util;
+
+import com.example.backend.model.Location;
+import java.util.List;
+
+public class DistanceUtils {
+
+    // Calculates straight-line Euclidean distance between two locations
+    public static double calculateDistance(Location loc1, Location loc2) {
+        double dLat = loc1.getLatitude() - loc2.getLatitude();
+        double dLon = loc1.getLongitude() - loc2.getLongitude();
+        return Math.sqrt(dLat * dLat + dLon * dLon);
+    }
+
+    // Builds an N x N distance matrix for fast algorithmic lookups
+    public static double[][] buildDistanceMatrix(List<Location> locations) {
+        int n = locations.size();
+        double[][] matrix = new double[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = calculateDistance(locations.get(i), locations.get(j));
+            }
+        }
+
+        return matrix;
+    }
+}
